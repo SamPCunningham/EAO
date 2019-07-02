c9fcf7bd61919c9060101efbe970654c37173328
Samuel Cunningham <samuel.p.cunningham@gmail.com>
2019-07-02 22:03:08 +0000

    added notification mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@eatinout-sam-cunningham.herokuapp.com"
 
-  def comment_added
-    mail(to: "my@email.com",
-          subect: "A comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+          subect: "A comment has been added to #{@place.name}")
   end
 end
